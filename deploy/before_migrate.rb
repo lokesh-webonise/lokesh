e9172ad66480a7f3460c1cbef6c95eb7d1188af2
lokesh-webonise <lokeshjawane@gmail.com>
2015-09-02 17:52:01 +0530

    sfdsdf

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -5,5 +5,5 @@ end
 template "#{release_path}/wp-config.php" do
   source "test.erb"
   cookbook "webonise_projects"
-  variables({:adapter => node['deploy']['app_name'], :host => "#{host}", :username => "#{username}", :password => "#{password}", :database => "#{database}"})
+  variables({:adapter => node['deploy']['app_name'], :host => "#{@host}", :username => "#{@username}", :password => "#{@password}", :database => "#{@database}"})
 end
