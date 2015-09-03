5f3a1927b0a613e60227607c1526312ff5632fc0
lokesh-webonise <lokeshjawane@gmail.com>
2015-09-03 12:40:35 +0530

    sfdsdffdfgsdf

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,3 +1,10 @@
+test = Chef::EncryptedDataBagItem.load("credentials", "abc")
+adapter = test["adapter"]
+host = test["host"]
+username = test["username"]
+password = test["password"]
+database = test["database"]
+
 file "/tmp/lokesh" do
 	action :create
 end
