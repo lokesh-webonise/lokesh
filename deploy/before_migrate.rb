f962dc481a748f94a2ec35b18f95cb87e5515594
lokesh-webonise <lokesh@weboniselab.com>
2015-11-20 18:11:47 +0530

    sdfsdfs

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -2,8 +2,8 @@ file "/tmp/lokesh" do
 	action :create
 end
 
-template "#{release_path}/test.rb" do
-  source "test.erb"
-  cookbook "webonise_projects"
-  variables({:adapter => node['deploy']['app_name']})
-end
+#template "#{release_path}/test.rb" do
+#  source "test.erb"
+#  cookbook "webonise_projects"
+#  variables({:adapter => node['deploy']['app_name']})
+#end
